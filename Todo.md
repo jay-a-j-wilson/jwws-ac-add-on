01034e41ff791fa91ad959b3a2d77e3fcbf8298c
Jay Wilson <jayanthonyjohnwilson@gmail.com>
2023-07-23 22:12:35 +1000

    3.3.4

---
+++ Todo.md
@@ -0,0 +1,14 @@
+# TODO
+
+Discount:
+- Add trash check for builder.
+- Add for variable product.
+
+Steps Settings:
+- Wrap steps in accordion.
+
+Default Cart Content:
+- Fix br tag logic.
+- Add leading zeros to ids.
+
+Add custom columns for wizard settings
---
+++ dist/jwws-ac-add-on/jwws-ac-add-on.php
@@ -0,0 +1,46 @@
+<?php declare(strict_types=1);
+
+namespace JWWS\ACA;
+
+use JWWS\ACA\App\Factory\Factory as App_Factory;
+
+/**
+ * Plugin Name:  Admin Columns - Add On
+ * Description:  Adds new columns.
+ * Version:      3.3.4
+ * Requires PHP: 8.1
+ * Author:       Jay Wilson
+ * License:      GPLv2 or later
+ * License URI:  http://www.gnu.org/licenses/gpl-2.0.html.
+ */
+if (! defined(constant_name: 'ABSPATH')) {
+    exit; // Exit if accessed directly.
+}
+
+require __DIR__ . '/vendor/autoload.php';
+
+\define(__NAMESPACE__ . '\DOMAIN', 'jwws');
+\define(__NAMESPACE__ . '\VENDOR_PREFIX', 'jwws__');
+
+\define(
+    constant_name: __NAMESPACE__ . '\PLUGIN_DIR',
+    value: basename(
+        path: plugin_dir_path(
+            file: __FILE__,
+        ),
+    ),
+);
+
+\define(
+    constant_name: __NAMESPACE__ . '\ASSETS_URL',
+    value: plugin_dir_url(file: __FILE__) . '/src/assets',
+);
+\define(
+    constant_name: __NAMESPACE__ . '\ASSETS_PATH',
+    value: plugin_dir_path(file: __FILE__) . 'src/assets',
+);
+
+App_Factory::new_instance()
+    ->create()
+    ->hook()
+;
